1359a3917b840c7a2c3724560443cc246c7facb7
Marina Strukova <meribsv@gmail.com>
2024-11-29 20:16:10 +0200

    Update main.js

--- module_11/lesson_22/main.js
+++ module_11/lesson_22/main.js
@@ -1,3 +1,4 @@
+// : вкінці параметру це динамічний параметр
 const BASE_URL = "http://localhost:3001/todos"
 
 const form = document.querySelector(".todo-form");
@@ -39,10 +40,12 @@ function handleSubmit(event) {
     const { todo } = event.target.elements;
 
     fetchData(BASE_URL, {
+// відповідає за створення даних
         method: "POST",
         headers: {
-            "Content-Type": "application/json"
+            "Content-Type": "application/json" // це службова інфа  - ми кажемо серверу ми передаємо дані в json форматі, але часто сервери розуміють і цей рядок не просять
         },
+//ОБОВ'ЯЗКОВО передаємо дані на сервер у форматі рядка!!. Ключі є в записі окремо
         body: JSON.stringify({ title: todo.value, completed: false })
     })
         .then(response => {
@@ -62,9 +65,10 @@ function handleUpdate(event) {
     const parent = event.target.closest(".list__item");
     const id = parent.dataset.id;
     
+//   /${id} - елемент який я хочу змінити  
     fetchData(`${BASE_URL}/${id}`, {
         method: "PATCH",
-        body: JSON.stringify({ completed: event.target.checked })
+        body: JSON.stringify({ completed: event.target.checked }) // яке саме значення я хочу змінити!!
     })
         .then(data => event.target.checked = data.completed)
         .catch(error => console.log(error))
@@ -77,11 +81,29 @@ function handleDelete(event) {
 
     const parent = event.target.closest(".list__item");
     const id = parent.dataset.id;
-    
 
+
+   
+    
+//робимо запит на сервер для видалення даних з бази даних по id
     fetchData(`${BASE_URL}/${id}`, {
         method: "DELETE"
     })
         .then(data => parent.remove())
         .catch(error => console.log(error))
 }
+
+/**
+*===========================
+*/
+//   /${id} - елемент який я хочу знайти та вставити нові данні, а всі дані що були видаляться!  
+//     fetchData(`${BASE_URL}/${id}`, {
+//          method: "PUT",
+//         body: JSON.stringify({ completed: event.target.checked })
+//     })
+//         .then(data => event.target.checked = data.completed)
+//         .catch(error => console.log(error))
+// }
+
+//методи "PUT" або "PATCH" використовуємо згідно документації, але частіше краще брати спред-оператор і властивістю перезаписати
+
